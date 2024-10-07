3d7d85ebb9e898cd1619d6d334be6d74aecafe7a
“Keaton <kshawhan@ucsc.edu>
2024-10-06 18:09:06 -0700

    step 1 done

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = '🤑';
+button.style.height = "100%";
+button.style.fontSize = "500%";
+button.style.backgroundColor = '#242424';
+app.append(button);

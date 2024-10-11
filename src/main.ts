c7244bddffe1a0676061a5006168ae7bd2f4090d
“Keaton <kshawhan@ucsc.edu>
2024-10-10 22:15:17 -0700

    Step 9 Complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,27 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+interface Item {
+  name: string,
+  cost: number,
+  rate: number,
+  emoji: string
+};
+
+const availableItems : Item[] = [
+  {name: "Baby", cost: 10, rate: 0.1, emoji: "👶"},
+  {name: "Toddler", cost: 100, rate: 2, emoji: "👦"},
+  {name: "Man", cost: 1000, rate: 50, emoji: "👨"},
+];
+
+const upgradeCounts: {[key: string]: number} = {
+  Baby: 0,
+  Toddler: 0,
+  Man: 0,
+};
+
+const buttons: {[key: string]: HTMLButtonElement} = {};
+
 const button = document.createElement("button");
 button.innerHTML = "👤";
 button.style.height = "100%";
@@ -16,24 +37,37 @@ button.style.fontSize = "500%";
 button.style.backgroundColor = "#242424";
 app.append(button);
 
+button.addEventListener("click", function () {
+  amount += 1;
+});
+
 let amount = 0;
 const counter = document.createElement("div");
 counter.innerHTML = "People: " + amount.toString();
 counter.style.fontSize = "24px";
 app.append(counter);
 
-function updateAmount() {
-  counter.innerHTML = "People: " + amount.toFixed(0).toString();
-  //counter.innerHTML = "People: " + amount.toString();
+let lastFrameTime = performance.now();
+
+function getTotalPassiveRate() {
+  return availableItems.reduce((total, item) => {
+    return total + upgradeCounts[item.name] * item.rate;
+  }, 0);
 }
 
-button.addEventListener("click", function () {
-  amount += 1;
-  updateAmount();
-  updateAutoDisable();
-});
+function updateAmount(time: number) {
+  const deltaTime = (time - lastFrameTime) / 1000;
+  lastFrameTime = time;
 
-const lastFrameTime = performance.now();
+  const passiveRate = getTotalPassiveRate();
+  amount += passiveRate * deltaTime;
+  counter.innerHTML = "People: " + amount.toFixed(0).toString();
+  passivePeople.innerHTML = "per Second: " + autoClickUpgrades.toString();
+  updateAutoDisable();
+  requestAnimationFrame(updateAmount);
+}
+
+requestAnimationFrame(updateAmount);
 
 let autoClickUpgrades = 0;
 const passivePeople = document.createElement("div");
@@ -42,185 +76,49 @@ passivePeople.style.fontSize = "16px";
 passivePeople.style.marginBottom = "10px";
 app.append(passivePeople);
 
-setInterval(() => autoClick(autoClickUpgrades), lastFrameTime);
+availableItems.forEach((item) => {
+  const buyUpgradeButton = document.createElement("button");
+  buyUpgradeButton.style.margin = "10px";
+  buyUpgradeButton.style.width = "220px";
+  buyUpgradeButton.style.fontSize = "180%";
+  buyUpgradeButton.style.height = "100px";
+  buyUpgradeButton.innerHTML = `${item.emoji} ${item.name}`;
 
-function autoClick(upgrades: number) {
-  amount += upgrades / (1000 / lastFrameTime);
-  updateAmount();
-  updateAutoDisable();
-  updatePassivePeople();
-}
-
-function updatePassivePeople() {
-  passivePeople.innerHTML = "per Second: " + autoClickUpgrades.toString();
-
-  passivePeople.style.fontSize = "12px";
-
-  tooltip.innerHTML =
-    "0.1 passive People (Cost: " +
-    smallUpgradePrice.toString() +
-    " People)<br>Purchased: " +
-    smallUpgradeCount.toString();
-
-  mediumTooltip.innerHTML =
-    "2 passive People (Cost: " +
-    mediumUpgradePrice.toString() +
-    " People)<br>Purchased: " +
-    mediumUpgradeCount.toString();
-
-  bigTooltip.innerHTML =
-    "50 passive People (Cost: " +
-    bigUpgradePrice.toString() +
-    " People)<br>Purchased: " +
-    bigUpgradeCount.toString();
-}
-
-const buySmallUpgrade = document.createElement("button");
-buySmallUpgrade.style.margin = "10px";
-buySmallUpgrade.style.maxWidth = "220px";
-buySmallUpgrade.style.height = "100px";
-buySmallUpgrade.style.minWidth = "220px";
-
-let smallUpgradeCount = 0;
-let smallUpgradePrice = 10;
-buySmallUpgrade.innerHTML = "👶 Baby";
-buySmallUpgrade.style.fontSize = "200%";
-
-const tooltip = document.createElement("div");
-tooltip.style.position = "absolute";
-tooltip.style.visibility = "hidden";
-tooltip.style.backgroundColor = "#242424";
-tooltip.style.color = "white";
-tooltip.style.padding = "5px";
-tooltip.style.borderRadius = "5px";
-tooltip.style.maxWidth = "200px";
-tooltip.innerHTML =
-  "0.1 passive People (Cost: " +
-  smallUpgradePrice.toString() +
-  " People)<br>Purchased: " +
-  smallUpgradeCount.toString();
-app.append(tooltip);
-
-buySmallUpgrade.addEventListener("mouseover", (event) => {
-  tooltip.style.visibility = "visible";
-  tooltip.style.left = event.pageX + "px";
-  tooltip.style.top = event.pageY + "px";
-});
-
-buySmallUpgrade.addEventListener("mouseout", () => {
+  const tooltip = document.createElement("div");
+  tooltip.style.position = "absolute";
   tooltip.style.visibility = "hidden";
-});
+  tooltip.style.backgroundColor = "#242424";
+  tooltip.style.color = "white";
+  tooltip.style.padding = "5px";
+  tooltip.style.borderRadius = "5px";
+  tooltip.style.maxWidth = "200px";
+  
+  tooltip.innerHTML = `Buy ${item.rate} passive People (Cost: ${item.cost} People)<br>Purchased: ${upgradeCounts[item.name]}`;
+  
+  buyUpgradeButton.addEventListener("mouseover", (event) => {
+    tooltip.style.visibility = "visible";
+    tooltip.style.left = event.pageX + 10 + "px";
+    tooltip.style.top = event.pageY + 10 + "px";
+  });
 
-app.append(buySmallUpgrade);
+  buyUpgradeButton.addEventListener("mouseout", () => {
+    tooltip.style.visibility = "hidden";
+  });
+
+  buyUpgradeButton.addEventListener("click", () => {
+    upgradeCounts[item.name]++;
+    autoClickUpgrades += item.rate;
+    amount -= item.cost; // Deduct the cost from the amount
+    tooltip.innerHTML = `Buy ${item.rate} passive People (Cost: ${item.cost} People)<br>Purchased: ${upgradeCounts[item.name]}`;
+  });
+  buttons[item.name] = buyUpgradeButton;
+
+  app.append(buyUpgradeButton);
+  app.append(tooltip);
+});
 
 function updateAutoDisable() {
-  buySmallUpgrade.disabled = amount >= smallUpgradePrice ? false : true;
-  buyMediumUpgrade.disabled = amount >= mediumUpgradePrice ? false : true;
-  buyBigUpgrade.disabled = amount >= bigUpgradePrice ? false : true;
+  availableItems.forEach((item) => {
+    buttons[item.name].disabled = amount >= item.cost ? false : true;
+  });
 }
-
-buySmallUpgrade.addEventListener("click", function () {
-  amount -= 10;
-  smallUpgradePrice *= 1.15;
-  autoClickUpgrades += 0.1;
-  smallUpgradeCount += 1;
-  updateAmount();
-  updateAutoDisable();
-  updatePassivePeople();
-});
-
-const buyMediumUpgrade = document.createElement("button");
-buyMediumUpgrade.style.margin = "10px";
-buyMediumUpgrade.style.maxWidth = "220px";
-buyMediumUpgrade.style.height = "100px";
-buyMediumUpgrade.style.fontSize = "180%";
-buyMediumUpgrade.style.minWidth = "220px";
-
-let mediumUpgradeCount = 0;
-let mediumUpgradePrice = 100;
-buyMediumUpgrade.innerHTML = "👦 Toddler";
-
-const mediumTooltip = document.createElement("div");
-mediumTooltip.style.position = "absolute";
-mediumTooltip.style.visibility = "hidden";
-mediumTooltip.style.backgroundColor = "#242424";
-mediumTooltip.style.color = "white";
-mediumTooltip.style.padding = "5px";
-mediumTooltip.style.borderRadius = "5px";
-mediumTooltip.style.maxWidth = "200px";
-mediumTooltip.innerHTML =
-  "2 passive People (Cost: " +
-  mediumUpgradePrice.toString() +
-  " People)<br>Purchased: " +
-  mediumUpgradeCount.toString();
-app.append(mediumTooltip);
-
-buyMediumUpgrade.addEventListener("mouseover", (event) => {
-  mediumTooltip.style.visibility = "visible";
-  mediumTooltip.style.left = event.pageX + 10 + "px";
-  mediumTooltip.style.top = event.pageY + 10 + "px";
-});
-
-buyMediumUpgrade.addEventListener("mouseout", () => {
-  mediumTooltip.style.visibility = "hidden";
-});
-
-app.append(buyMediumUpgrade);
-
-buyMediumUpgrade.addEventListener("click", function () {
-  amount -= 100;
-  mediumUpgradePrice *= 1.15;
-  autoClickUpgrades += 2;
-  mediumUpgradeCount += 1;
-  updateAmount();
-  updateAutoDisable();
-  updatePassivePeople();
-});
-
-const buyBigUpgrade = document.createElement("button");
-buyBigUpgrade.style.margin = "10px";
-buyBigUpgrade.style.maxWidth = "220px";
-buyBigUpgrade.style.height = "100px";
-buyBigUpgrade.style.fontSize = "180%";
-buyBigUpgrade.style.minWidth = "220px";
-
-let bigUpgradeCount = 0;
-let bigUpgradePrice = 100;
-buyBigUpgrade.innerHTML = "👨 Man";
-
-const bigTooltip = document.createElement("div");
-bigTooltip.style.position = "absolute";
-bigTooltip.style.visibility = "hidden";
-bigTooltip.style.backgroundColor = "#242424";
-bigTooltip.style.color = "white";
-bigTooltip.style.padding = "5px";
-bigTooltip.style.borderRadius = "5px";
-bigTooltip.style.maxWidth = "200px";
-bigTooltip.innerHTML =
-  "50 passive People (Cost: " +
-  bigUpgradePrice.toString() +
-  " People)<br>Purchased: " +
-  bigUpgradeCount.toString();
-app.append(bigTooltip);
-
-buyBigUpgrade.addEventListener("mouseover", (event) => {
-  bigTooltip.style.visibility = "visible";
-  bigTooltip.style.left = event.pageX + 10 + "px";
-  bigTooltip.style.top = event.pageY + 10 + "px";
-});
-
-buyBigUpgrade.addEventListener("mouseout", () => {
-  bigTooltip.style.visibility = "hidden";
-});
-
-app.append(buyBigUpgrade);
-
-buyBigUpgrade.addEventListener("click", function () {
-  amount -= 1000;
-  bigUpgradePrice *= 1.15;
-  autoClickUpgrades += 50;
-  bigUpgradeCount += 1;
-  updateAmount();
-  updateAutoDisable();
-  updatePassivePeople();
-});
